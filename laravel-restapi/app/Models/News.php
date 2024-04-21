91d46dbc6af7d2b36a98958ad69b2e0e13375e73
azizHr <azizharkati20@gmail.com>
2024-04-21 14:57:26 +0100

    add fillable to News model

--- laravel-restapi/app/Models/News.php
+++ laravel-restapi/app/Models/News.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'started_at',
+        'expired_at',
+        'category_id'
+    ]
 }
